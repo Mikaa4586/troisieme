77ec3fef5946c98d1c70853d587ec5315408d6f3
Mikaa4586 <135849287+Mikaa4586@users.noreply.github.com>
2023-10-03 11:55:00 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+class MusicPlayer
+{
+    constructor(format)
+    {
+        this.format = format;
+        document.write(format);
+    }
+}
+let music = new MusicPlayer("Nvidia <br>");
+let music3 = new MusicPlayer("LOl");
